813f4eec3041c1e65a46b003d1b18859f8a5112e
Adam Amster <adam.amster@alleninstitute.org>
2023-10-13 17:16:42 -0400

    add start, end to predict

--- src/detect_sleep_states/classify_segment/model.py
+++ src/detect_sleep_states/classify_segment/model.py
@@ -130,7 +130,9 @@ class ClassifySegmentModel(pytorch_lightning.LightningModule):
         scores = torch.nn.functional.softmax(logits, dim=1)
         res = [{
             'pred': label_id_str_map[preds[i].item()],
-            'score': scores[i][preds[i]].item()
+            'score': scores[i][preds[i]].item(),
+            'start': data['start'][i].item(),
+            'end': data['end'][i].item()
         } for i in range(len(preds))]
         return res
 
