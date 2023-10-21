add code to average overlapping predictions and other prior fixes and changes

src/detect_sleep_states/classify_segment/dataset.py
@@ -31,7 +31,8 @@ class ClassifySegmentDataset(torch.utils.data.Dataset):
         transform,
         sequence_length: int = 720,
         is_train: bool = True,
-        limit_to_series_ids: Optional[List] = None
+        limit_to_series_ids: Optional[List] = None,
+        load_series: bool = True
     ):
         """
 
@@ -45,13 +46,19 @@ class ClassifySegmentDataset(torch.utils.data.Dataset):
         :param transform:
         :param limit_to_series_ids
             Limit to loading these series ids
+        :param load_series
+            Whether to load series. It takes a while, skip if not needed
         """
         super().__init__()
 
         filters = [('series_id', 'in', limit_to_series_ids)] if (
                 limit_to_series_ids is not None) else None
-        series = pd.read_parquet(data_path, filters=filters)
-        series = series.sort_values(['series_id', 'step'])
+
+        if load_series:
+            series = pd.read_parquet(data_path, filters=filters)
+            series = series.sort_values(['series_id', 'step'])
+        else:
+            series = None
 
         self._series = series
         self._meta = meta
@@ -120,6 +127,10 @@ class ClassifySegmentDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return self._meta.shape[0]
+
+    @property
+    def meta(self):
+        return self._meta
 
 
 def is_valid_sequence(seq_meta: pd.Series, sequence_length: int):

src/detect_sleep_states/classify_segment/model.py
@@ -1,7 +1,6 @@
 from typing import Dict, Any
 
 import lightning
-import pytorch_lightning
 import torch
 import torchmetrics
 from torch import nn
@@ -64,7 +63,7 @@ class DetectSleepModel(nn.Module):
         return x
 
 
-class ClassifySegmentModel(pytorch_lightning.LightningModule):
+class ClassifySegmentModel(lightning.pytorch.LightningModule):
     def __init__(
         self,
         learning_rate,
@@ -130,9 +129,7 @@ class ClassifySegmentModel(pytorch_lightning.LightningModule):
         scores = torch.nn.functional.softmax(logits, dim=1)
         res = [{
             'pred': label_id_str_map[preds[i].item()],
-            'score': scores[i][preds[i]].item(),
-            'start': data['start'][i].item(),
-            'end': data['end'][i].item()
+            'scores': scores[i]
         } for i in range(len(preds))]
         return res
 