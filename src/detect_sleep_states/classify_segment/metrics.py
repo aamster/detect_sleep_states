62ad6b67eb2568cceb3a9819ee342b13922fc506
Adam Amster <adam.amster@alleninstitute.org>
2023-10-27 08:23:38 -0400

    undo logic to match up preds/events. Just calc metrics

--- src/detect_sleep_states/classify_segment/metrics.py
+++ src/detect_sleep_states/classify_segment/metrics.py
@@ -1,7 +1,6 @@
 from typing import Union
 
 import pandas as pd
-import torch
 
 from detect_sleep_states.util import clean_events
 
@@ -10,15 +9,6 @@ def calc_accuracy(
     preds: pd.DataFrame,
     events: Union[pd.DataFrame, str]
 ):
-    label_idx_map = {
-        'no_event': 0,
-        'onset': 1,
-        'wakeup': 2
-    }
-
-    all_preds = []
-    all_events = []
-
     if not isinstance(events, pd.DataFrame):
         events = clean_events(events_path=events)
     if events.index.name != 'series_id':
@@ -26,11 +16,14 @@ def calc_accuracy(
 
     if preds.index.name != 'series_id':
         preds = preds.set_index('series_id')
+
+    TP = 0
+    FP = 0
+    FN = 0
+
     for series_id in events.index.unique():
         series_events = events.loc[[series_id]]
         series_preds = preds.loc[[series_id]]
-        series_preds_corrected = series_preds.to_dict(orient='records')
-        series_events_corrected = series_events.to_dict(orient='records')
 
         event_matches = dict()
         for event_idx in range(series_events.shape[0]):
@@ -39,7 +32,8 @@ def calc_accuracy(
         for pred_idx, pred in enumerate(series_preds.itertuples()):
             for event_idx, event in enumerate(series_events.itertuples()):
                 if pred.start <= event.step <= pred.end:
-                    event_matches[event_idx].add(pred_idx)
+                    if pred.pred == event.event:
+                        event_matches[event_idx].add(pred_idx)
 
         pred_matches = dict()
         for pred_idx in range(series_preds.shape[0]):
@@ -48,92 +42,31 @@ def calc_accuracy(
         for event_idx, event in enumerate(series_events.itertuples()):
             for pred_idx, pred in enumerate(series_preds.itertuples()):
                 if pred.start <= event.step <= pred.end:
-                    pred_matches[pred_idx].add(event_idx)
+                    if pred.pred == event.event:
+                        pred_matches[pred_idx].add(event_idx)
 
         for pred_idx, event_idx_matches in pred_matches.items():
-            if len(event_idx_matches) == 1:
-                continue
-            to_insert = {
-                'series_id': series_id,
-                'event': 'no_event',
-                'step': None
-            }
+            TP += len(event_idx_matches)
             if len(event_idx_matches) == 0:
-                pred = series_preds.iloc[pred_idx]
-                i = 0
-                inserted = False
-                while i < len(series_events_corrected):
-                    if series_events_corrected[i]['event'] == 'no_event':
-                        while (i < len(series_events_corrected) and
-                               series_events_corrected[i]['event'] == 'no_event'):
-                            i += 1
-                    if i == len(series_events_corrected):
-                        if series_events_corrected[-1]['event'] == 'no_event':
-                            series_events_corrected.append(to_insert)
-                            inserted = True
-                            break
-                    else:
-                        if pred['end'] <= series_events_corrected[i]['step']:
-                            series_events_corrected.insert(i, to_insert)
-                            inserted = True
-                            break
-                    i += 1
-
-                if not inserted:
-                    series_events_corrected.append(to_insert)
-
-            else:
-                raise NotImplementedError(
-                    'found multiple gt matches for a pred')
+                FP += 1
 
         for event_idx, pred_idx_matches in event_matches.items():
-            if len(pred_idx_matches) == 1:
-                continue
             if len(pred_idx_matches) == 0:
-                event = series_events.iloc[event_idx]
-                to_insert = {
-                    'series_id': series_id,
-                    'pred': 'no_event',
-                    'start': None,
-                    'end': None
-                }
-                i = 0
-                inserted = False
-                while i < len(series_preds_corrected):
-                    if series_preds_corrected[i]['pred'] == 'no_event':
-                        while (i < len(series_preds_corrected) and
-                               series_preds_corrected[i]['pred'] == 'no_event'):
-                            i += 1
-                    if i == len(series_preds_corrected):
-                        if series_preds_corrected[-1]['pred'] == 'no_event':
-                            series_preds_corrected.append(to_insert)
-                            inserted = True
-                            break
-                    else:
-                        if event['step'] <= series_preds_corrected[i]['start']:
-                            series_preds_corrected.insert(i, to_insert)
-                            inserted = True
-                            break
-                    i += 1
+                FN += 1
 
-                if not inserted:
-                    series_preds_corrected.append(to_insert)
-            else:
-                raise NotImplementedError(
-                    'Found multiple pred matches for a gt')
+    p = TP / (TP + FP)
+    r = TP / (TP + FN)
 
-        all_preds += [label_idx_map[x['pred']] for x in series_preds_corrected]
-        all_events += [label_idx_map[x['event']] for x in series_events_corrected]
+    if p + r == 0:
+        f1 = 0.0
+    else:
+        f1 = 2 * p * r / (p + r)
 
-    all_preds = torch.tensor(all_preds)
-    all_events = torch.tensor(all_events)
-
-    tp = (all_preds == all_events).sum().item()
-    fp = ((all_preds != 0) & (all_preds != all_events)).sum().item()
-    fn = ((all_events != 0) & (all_preds != all_events)).sum().item()
-
-    p = tp / (tp + fp)
-    r = tp / (tp + fn)
-    f1 = 2 * p * r / (p + r)
-
-    return p, r, f1, all_preds, all_events
+    return {
+        'precision': p,
+        'recall': r,
+        'f1': f1,
+        'TP': TP,
+        'FP': FP,
+        'FN': FN
+    }
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -22,47 +22,60 @@ def test_calc_accuracy_all_correct():
         'step': [50, 250]
     })
 
-    p, r, f1, preds, events = \
+    res = \
         calc_accuracy(
             preds=preds,
             events=events)
-    assert preds == [1, 2]
-    assert events == [1, 2]
+    assert res['TP'] == 2
+    assert res['FP'] == 0
+    assert res['FN'] == 0
 
 
 @pytest.mark.parametrize(
-    'start, end, pred, expected_preds, expected_events', [
+    'start, end, pred, expected', [
         pytest.param(
     [100, 200, 330, 400],
             [175, 325, 375, 475],
             ['wakeup', 'onset', 'wakeup', 'onset'],
-            [2, 1, 2, 1],
-            [1, 0, 2, 1]
+            {
+                'TP': 2,
+                'FP': 2,
+                'FN': 1
+            }
         ),
         pytest.param(
     [50, 100, 300, 400],
             [75, 200, 400, 500],
             ['wakeup', 'onset', 'wakeup', 'onset'],
-            [2, 1, 2, 1],
-            [0, 1, 2, 1]
+            {
+                'TP': 3,
+                'FP': 1,
+                'FN': 0
+            }
         ),
         pytest.param(
     [100, 300, 400, 1000],
             [200, 400, 500, 2000],
             ['onset', 'wakeup', 'onset', 'wakeup'],
-            [1, 2, 1, 2],
-            [1, 2, 1, 0]
+            {
+                'TP': 1 + 1 + 1,
+                'FP': 1,
+                'FN': 0
+            }
         ),
         pytest.param(
     [100, 250, 300, 325, 425, 1000],
             [200, 300, 325, 400, 475, 2000],
             ['onset', 'wakeup', 'onset', 'wakeup', 'onset', 'wakeup'],
-            [1, 2, 1, 2, 1, 2],
-            [1, 0, 0, 2, 1, 0]
+            {
+                'TP': 1 + 1 + 1,
+                'FP': 1 + 1 + 1,
+                'FN': 0
+            }
         )
     ]
 )
-def test_calc_accuracy_fp(start, end, pred, expected_preds, expected_events):
+def test_calc_accuracy_fp(start, end, pred, expected):
     preds = pd.DataFrame({
         'series_id': ['series1'] * len(pred),
         'pred': pred,
@@ -76,43 +89,56 @@ def test_calc_accuracy_fp(start, end, pred, expected_preds, expected_events):
         'event': ['onset', 'wakeup', 'onset'],
         'step': [150, 350, 450]
     })
-    p, r, f1, preds, events = \
+    res = \
         calc_accuracy(
             preds=preds,
             events=events)
-    assert preds == expected_preds
-    assert events == expected_events
+    assert res['TP'] == expected['TP']
+    assert res['FP'] == expected['FP']
+    assert res['FN'] == expected['FN']
 
 
 @pytest.mark.parametrize(
-    'step, event, expected_preds, expected_events', [
+    'step, event, expected', [
         pytest.param(
             [75, 250, 400],
             ['onset', 'wakeup', 'onset'],
-            [1, 0, 2],
-            [1, 2, 1]
+            {
+                'TP': 1,
+                'FP': 1,
+                'FN': 2
+            }
         ),
         pytest.param(
             [25, 75, 375],
             ['onset', 'wakeup', 'onset'],
-            [0, 1, 2],
-            [1, 2, 1]
+            {
+                'TP': 0,
+                'FP': 2,
+                'FN': 1 + 1 + 1
+            }
         ),
         pytest.param(
             [75, 375, 1000],
             ['onset', 'wakeup', 'onset'],
-            [1, 2, 0],
-            [1, 2, 1]
+            {
+                'TP': 1 + 1,
+                'FP': 0,
+                'FN': 1
+            }
         ),
         pytest.param(
             [75, 250, 300, 400, 1000],
             ['onset', 'wakeup', 'onset', 'wakeup', 'onset'],
-            [1, 0, 0, 2, 0],
-            [1, 2, 1, 2, 1]
+            {
+                'TP': 1 + 1,
+                'FP': 0,
+                'FN': 1 + 1 + 1
+            }
         )
     ]
 )
-def test_calc_accuracy_fn(step, event, expected_preds, expected_events):
+def test_calc_accuracy_fn(step, event, expected):
     preds = pd.DataFrame({
         'series_id': ['series1', 'series1'],
         'pred': ['onset', 'wakeup'],
@@ -126,12 +152,13 @@ def test_calc_accuracy_fn(step, event, expected_preds, expected_events):
         'event': event,
         'step': step
     })
-    p, r, f1, preds, events = \
+    res = \
         calc_accuracy(
             preds=preds,
             events=events)
-    assert preds == expected_preds
-    assert events == expected_events
+    assert res['TP'] == expected['TP']
+    assert res['FP'] == expected['FP']
+    assert res['FN'] == expected['FN']
 
 
 def test_calc_accuracy_all_errors():
@@ -149,12 +176,13 @@ def test_calc_accuracy_all_errors():
         'step': [150, 251, 300]
     })
 
-    p, r, f1, preds, events = \
+    res = \
         calc_accuracy(
             preds=preds,
             events=events)
-    assert preds == [1, 0, 0, 0]
-    assert events == [0, 1, 2, 1]
+    assert res['TP'] == 0
+    assert res['FP'] == 1
+    assert res['FN'] == 3
 
 
 def test_calc_accuracy_multiple_pred_matches():
@@ -171,33 +199,33 @@ def test_calc_accuracy_multiple_pred_matches():
         'event': ['onset', 'wakeup'],
         'step': [50, 75]
     })
-    p, r, f1, preds, events = \
+    res = \
         calc_accuracy(
             preds=preds,
             events=events)
-    assert preds == [1, 0]
-    assert events == [1, 2]
-#
-#
-# def test_calc_accuracy_multiple_event_matches():
-#     preds = pd.DataFrame({
-#         'series_id': ['series1', 'series1', 'series1'],
-#         'pred': ['onset', 'wakeup', 'onset'],
-#         'start': [25, 55, 65],
-#         'end': [70, 125, 200]
-#     })
-#
-#     events = pd.DataFrame({
-#         'series_id': ['series1', 'series1', 'series1'],
-#         'night': [1, 1, 1],
-#         'event': ['onset', 'wakeup', 'onset'],
-#         'step': [50, 75, 100]
-#     })
-#     with (tempfile.TemporaryDirectory() as tmpdir):
-#         events.to_csv(Path(tmpdir) / 'events.csv', index=False)
-#         p, r, f1, preds, events = \
-#             calc_accuracy(
-#                 preds=preds,
-#                 events_path=str(Path(tmpdir) / 'events.csv'))
-#         assert preds == [1, 2, 1]
-#         assert events == [1, 2, 0]
+    assert res['TP'] == 1
+    assert res['FP'] == 0
+    assert res['FN'] == 1
+
+
+def test_calc_accuracy_multiple_event_matches():
+    preds = pd.DataFrame({
+        'series_id': ['series1', 'series1', 'series1'],
+        'pred': ['onset', 'wakeup', 'onset'],
+        'start': [25, 55, 65],
+        'end': [70, 125, 200]
+    })
+
+    events = pd.DataFrame({
+        'series_id': ['series1', 'series1'],
+        'night': [1, 1],
+        'event': ['onset', 'wakeup'],
+        'step': [50, 75]
+    })
+    res = \
+        calc_accuracy(
+            preds=preds,
+            events=events)
+    assert res['TP'] == 2
+    assert res['FP'] == 1
+    assert res['FN'] == 0
